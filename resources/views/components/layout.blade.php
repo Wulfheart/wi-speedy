cb66e612ca6879e404a8444cd9ebd22f6ea21911
wulfheart <dev@alexfwulf.de>
2020-10-14 22:58:09 +0200

    CSS minified

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,6 @@
             display: none;
         }
     </style>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
 
 </head>
 <!-- 👀 Nothing to see here, just an empty alpine component  -->
